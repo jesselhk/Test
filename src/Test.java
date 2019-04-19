323fdf17f9c40a57f3bce6ddf0090c9f1e950cd4
Buttercawan <couteautm@gmail.com>
2019-04-20 04:34:43 +1200

    Test

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,11 @@ public class Test {
 
     public static void main(String args[]) {
 
+        Server server = new Server();
+        Thread serverThread = new Thread(server);
+        serverThread.start();
+
+
         TestSingleton test = TestSingleton.getInstance();
 
 
